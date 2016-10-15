e782c7e97e03497bf492d7565279fb5e293f11a5
Branan Riley <branan@puppet.com>
2016-10-15 10:55:54 -0700

    Remove unnecessary read_bytes wrapper
    This was copied from an earlier pgp attempt that wrapped io::Errors into
    byteorder's error schema. That's no longer needed, so the read_exact can
    just be pulled directly into the bignum implementation, simplifying the
    code.

--- src/util.rs
+++ src/util.rs
@@ -2,14 +2,10 @@ use errors::*;
 use std::io;
 use byteorder::{BigEndian,ReadBytesExt};
 
-fn read_bytes<T: io::Read>(data: &mut T, len: usize) -> Result<Vec<u8>> {
-    let mut result: Vec<u8> = vec![0; len];
-    try!(data.read_exact(&mut result).chain_err(|| "Could not read packet bytes"));
-    Ok(result)
-}
-
 pub fn read_bignum<T: io::Read>(data: &mut T) -> Result<Vec<u8>> {
     let bit_len = try!(data.read_u16::<BigEndian>().chain_err(|| "Could not read size for bignum")) as usize;
     let len = (bit_len + 7) / 8;
-    read_bytes(data, len)
+    let mut result: Vec<u8> = vec![0; len];
+    try!(data.read_exact(&mut result).chain_err(|| "Could not read data for bignum"));
+    Ok(result)
 }
